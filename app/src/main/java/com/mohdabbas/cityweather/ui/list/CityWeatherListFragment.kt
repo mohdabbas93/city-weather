664604cc9ff8e47dbb0c347977e8f22ceb074425
Mohammad Abbas <mohdabbas@narbase.com>
2022-05-14 14:42:05 +0400

    Create CityWeatherListFragment with its layout and add it in nav graph as start destination

---
+++ app/src/main/java/com/mohdabbas/cityweather/ui/list/CityWeatherListFragment.kt
@@ -0,0 +1,11 @@
+package com.mohdabbas.cityweather.ui.list
+
+import androidx.fragment.app.Fragment
+import com.mohdabbas.cityweather.R
+
+/**
+ * Created by Mohammad Abbas
+ * On: 5/14/22.
+ */
+class CityWeatherListFragment : Fragment(R.layout.fragment_city_weather_list) {
+}
