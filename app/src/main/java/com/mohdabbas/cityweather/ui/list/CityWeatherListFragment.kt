fd15d2f41e0cfd71c25cad175a055934e4a36015
Mohammad Abbas <mohdabbas@narbase.com>
2022-05-15 14:56:30 +0400

    Setup the city weather recycler view - Set its layout manager - Set its adapter to the city weather adpater and pass an empty list to the adapter

--- app/src/main/java/com/mohdabbas/cityweather/ui/list/CityWeatherListFragment.kt
+++ app/src/main/java/com/mohdabbas/cityweather/ui/list/CityWeatherListFragment.kt
@@ -5,8 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.mohdabbas.cityweather.R
 import com.mohdabbas.cityweather.databinding.FragmentCityWeatherListBinding
+import com.mohdabbas.cityweather.ui.list.adapter.CityWeatherAdapter
 
 /**
  * Created by Mohammad Abbas
@@ -16,6 +18,8 @@ class CityWeatherListFragment : Fragment(R.layout.fragment_city_weather_list) {
     private var _binding: FragmentCityWeatherListBinding? = null
     private val binding get() = _binding!!
 
+    private val cityWeatherAdapter = CityWeatherAdapter(listOf())
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -23,7 +27,16 @@ class CityWeatherListFragment : Fragment(R.layout.fragment_city_weather_list) {
     ): View {
         _binding = FragmentCityWeatherListBinding.inflate(layoutInflater, container, false)
 
+        setupRecyclerView()
+
         return binding.root
+    }
+
+    private fun setupRecyclerView() {
+        binding.rvCityWeather.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = cityWeatherAdapter
+        }
     }
 
     override fun onDestroyView() {
