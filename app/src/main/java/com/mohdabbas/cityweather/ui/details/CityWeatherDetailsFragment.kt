40d9268c95977812bec6f89154f21f938f8d481a
Mohammad Abbas <mohdabbas@narbase.com>
2022-05-16 02:32:58 +0400

    Populate city weather details fragment views - Receive the city id through the generated args class - Get the city weather details from the SharedViewModel using the city id - Add string resources - Populate the views with the details

--- app/src/main/java/com/mohdabbas/cityweather/ui/details/CityWeatherDetailsFragment.kt
+++ app/src/main/java/com/mohdabbas/cityweather/ui/details/CityWeatherDetailsFragment.kt
@@ -1,14 +1,19 @@
 package com.mohdabbas.cityweather.ui.details
 
+//import com.mohdabbas.cityweather.ui.list.CityWeatherListFragmentArgs
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.mohdabbas.cityweather.R
+import com.mohdabbas.cityweather.data.CityWeather
 import com.mohdabbas.cityweather.databinding.FragmentCityWeatherDetailsBinding
 import com.mohdabbas.cityweather.ui.SharedViewModel
+import com.mohdabbas.cityweather.util.WeatherUtil.temperatureFromKelvinToCelsius
 import dagger.hilt.android.AndroidEntryPoint
 
 /**
@@ -30,6 +35,42 @@ class CityWeatherDetailsFragment : Fragment(R.layout.fragment_city_weather_detai
     ): View? {
         _binding = FragmentCityWeatherDetailsBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+
+    private val cityWeatherDetailsFragmentArgs: CityWeatherDetailsFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        sharedViewModel.getCityWeatherInfoByCityId(cityWeatherDetailsFragmentArgs.cityId)?.let {
+            populateViews(it)
+        }
+    }
+
+    private fun populateViews(cityWeather: CityWeather) {
+        binding.apply {
+            val weather = cityWeather.weather.firstOrNull()
+
+            tvWeatherCondition.text =
+                weather?.main ?: getString(R.string.unavailable_weather_condition)
+            tvTemp.text = cityWeather.main.temp.temperatureFromKelvinToCelsius().toString()
+
+            Glide.with(requireContext())
+                .load("http://openweathermap.org/img/wn/${weather?.icon ?: ""}@2x.png")
+                .centerCrop()
+                .placeholder(R.drawable.ic_launcher_background)
+                .into(ivWeatherIcon)
+
+            wind.tvTitle.text = getString(R.string.wind)
+            wind.tvValue.text = cityWeather.wind.speed.toInt().toString()
+
+            humidity.tvTitle.text = getString(R.string.humidity)
+            humidity.tvValue.text = cityWeather.main.humidity.toInt().toString()
+
+            pressure.tvTitle.text = getString(R.string.pressure)
+            pressure.tvValue.text = cityWeather.main.pressure.toInt().toString()
+        }
     }
 
     override fun onDestroyView() {
