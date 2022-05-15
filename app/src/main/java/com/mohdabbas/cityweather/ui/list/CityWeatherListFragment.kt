8bd292d348e8d7ac76d2a7c171da24a12cedf4ef
Mohammad Abbas <mohdabbas@narbase.com>
2022-05-15 14:52:42 +0400

    Setup the binding of the city weather list layout

--- app/src/main/java/com/mohdabbas/cityweather/ui/list/CityWeatherListFragment.kt
+++ app/src/main/java/com/mohdabbas/cityweather/ui/list/CityWeatherListFragment.kt
@@ -1,11 +1,33 @@
 package com.mohdabbas.cityweather.ui.list
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.mohdabbas.cityweather.R
+import com.mohdabbas.cityweather.databinding.FragmentCityWeatherListBinding
 
 /**
  * Created by Mohammad Abbas
  * On: 5/14/22.
  */
 class CityWeatherListFragment : Fragment(R.layout.fragment_city_weather_list) {
+    private var _binding: FragmentCityWeatherListBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentCityWeatherListBinding.inflate(layoutInflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
