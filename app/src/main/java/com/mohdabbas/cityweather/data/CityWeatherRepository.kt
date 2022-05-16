ba677f9975975c5f54ea6be119d82de7d728c50c
Mohammad Abbas <mohdabbas@narbase.com>
2022-05-16 07:03:11 +0400

    Add documentation

--- app/src/main/java/com/mohdabbas/cityweather/data/CityWeatherRepository.kt
+++ app/src/main/java/com/mohdabbas/cityweather/data/CityWeatherRepository.kt
@@ -4,11 +4,19 @@ import com.mohdabbas.cityweather.util.JsonParser
 import javax.inject.Inject
 
 /**
- * Created by Mohammad Abbas
- * On: 5/15/22.
+ * The repository to get the cities weather
+ *
+ * @author Mohammad Abbas
+ * @property jsonParser an instance of the [JsonParser] class.
  */
 class CityWeatherRepository @Inject constructor(private val jsonParser: JsonParser) {
 
+    /**
+     *  Uses to get the city by using the [JsonParser] class to get
+     *  the list of cities weather
+     *
+     * @return a list of [CityWeather].
+     */
     fun getCitiesWeather(): List<CityWeather> {
         return jsonParser.parseJson("weather_14.json")
     }
