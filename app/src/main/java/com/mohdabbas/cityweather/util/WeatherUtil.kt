1b20d2b26a192a8e2c4f6f7dae11fc83f300fb2d
Mohammad Abbas <mohdabbas@narbase.com>
2022-05-16 04:44:23 +0400

    Add documention

--- app/src/main/java/com/mohdabbas/cityweather/util/WeatherUtil.kt
+++ app/src/main/java/com/mohdabbas/cityweather/util/WeatherUtil.kt
@@ -1,15 +1,28 @@
 package com.mohdabbas.cityweather.util
 
 /**
- * Created by Mohammad Abbas
- * On: 5/15/22.
+ * Combination of utility functions
+ *
+ * @author Mohammad Abbas
  */
 object WeatherUtil {
 
+    /**
+     * Convert Temperature from Kelvin to Celsius
+     *
+     * @receiver a number of type [Double]
+     * @return The result of the conversation
+     */
     fun Double.temperatureFromKelvinToCelsius(): Int {
         return (this - 273.15).toInt()
     }
 
+    /**
+     * Convert speed from meter/sec to km/sec
+     *
+     * @receiver a number of type [Double]
+     * @return The result of the conversation
+     */
     fun Double.speedFromMeterPerSecToKmPerSec(): Int {
         return (this * 3.6).toInt()
     }
