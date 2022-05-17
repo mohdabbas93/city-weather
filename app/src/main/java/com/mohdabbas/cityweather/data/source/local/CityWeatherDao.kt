aaaf74a2f038c8138333d7cc75be49aa567eaf3f
Mohammad Abbas <mohdabbas@narbase.com>
2022-05-18 00:15:41 +0400

    Create city weather Dao - Create a Dao interface that has function to interact with the city_weather table

---
+++ app/src/main/java/com/mohdabbas/cityweather/data/source/local/CityWeatherDao.kt
@@ -0,0 +1,33 @@
+package com.mohdabbas.cityweather.data.source.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.mohdabbas.cityweather.data.source.local.entity.CityWeatherData
+
+/**
+ * Represent the city weather Dao that contain functions to interact
+ * with data in the [CityWeatherData] table
+ *
+ * @author Mohammad Abbas
+ */
+@Dao
+interface CityWeatherDao {
+
+    /**
+     *  The function used to insert/update cities weather
+     *
+     *  @param citiesWeather Cities weather to be inserted/updated
+     */
+    @Insert
+    suspend fun insertCitiesWeatherData(citiesWeather: List<CityWeatherData>)
+
+    /**
+     *  The function used to retrieve a list of cities weather from
+     *  cities_weather table
+     *
+     *  @return List of cities weather
+     */
+    @Query("SELECT * FROM city_weather")
+    suspend fun getCitiesWeatherData(): List<CityWeatherData>
+}
