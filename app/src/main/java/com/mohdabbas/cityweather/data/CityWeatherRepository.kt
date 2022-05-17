85403b3a9f1393a28fb821f23e499659c2a46f0c
Mohammad Abbas <mohdabbas@narbase.com>
2022-05-18 01:42:40 +0400

    Update getting cities weather functionality - Get the data from the database and if not empty mapped the result from CityWeatherData to CityWeather - If the result is empty then parsed the Json file in the assests and store in Db and get the data from db again an return it

--- app/src/main/java/com/mohdabbas/cityweather/data/CityWeatherRepository.kt
+++ app/src/main/java/com/mohdabbas/cityweather/data/CityWeatherRepository.kt
@@ -1,26 +1,51 @@
 package com.mohdabbas.cityweather.data
 
+import com.mohdabbas.cityweather.data.source.local.LocalDataSource
 import com.mohdabbas.cityweather.util.JsonParser
 import javax.inject.Inject
 
 /**
- * The repository to get the cities weather
+ * The repository to interact with the cities weather data
  *
  * @author Mohammad Abbas
+ * @property localDataSource an instance of the [LocalDataSource] class
  * @property jsonParser an instance of the [JsonParser] class.
+ * @property mapper an instance of the [Mapper] class
  */
-class CityWeatherRepository @Inject constructor(private val jsonParser: JsonParser) {
+class CityWeatherRepository @Inject constructor(
+    private val localDataSource: LocalDataSource,
+    private val jsonParser: JsonParser,
+    private val mapper: Mapper
+) {
 
     /**
-     *  Uses to get the city by using the [JsonParser] class to get
-     *  the list of cities weather
+     *  Used to get the list of [CityWeather] from the database.
+     *
+     *  If the result from the database is empty the data will be parsed
+     *  from the Json file in the assets folder and after that will be
+     *  stored in the database and get the list of [CityWeather] again.
      *
      * @return a list of [CityWeather].
      */
-    fun getCitiesWeather(): List<CityWeather> {
-        return jsonParser.parseJson("weather_14.json")
+    suspend fun getCitiesWeather(): List<CityWeather> {
+        val citiesWeather = localDataSource.getCitiesWeatherData()
+
+        return if (citiesWeather.isEmpty()) {
+            updateCityWeatherDataInDb()
+            mapper.run { localDataSource.getCitiesWeatherData().toCityWeather() }
+        } else {
+            mapper.run { citiesWeather.toCityWeather() }
+        }
+    }
+
+    /**
+     * Used to parse the list of [CityWeather] data from the Json file in the
+     * assets folder using [JsonParser] and mapped it to list of CityWeatherData, then update the
+     * database with the data
+     */
+    private suspend fun updateCityWeatherDataInDb() {
+        val parsedData = jsonParser.parseJson("weather_14.json")
+        val mappedData = mapper.run { parsedData.toCityWeatherData() }
+        localDataSource.storeCitiesWeatherDataInDb(mappedData)
     }
 }
-
-
-
