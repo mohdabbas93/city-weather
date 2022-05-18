dad8a10fb6e781f9b3d639ef1568ff1cf01764fa
Mohammad Abbas <mohdabbas@narbase.com>
2022-05-18 04:53:22 +0400

    Change how the database updated - Downlaod the gzip file from netwrok unsing the remote data source - Using the GZIPUtil to get the gzipInputStream from the downloaded file - Write to the internal storage and read again to parsed using Gson - Mapped the result and store in DB

--- app/src/main/java/com/mohdabbas/cityweather/data/CityWeatherRepository.kt
+++ app/src/main/java/com/mohdabbas/cityweather/data/CityWeatherRepository.kt
@@ -1,6 +1,10 @@
 package com.mohdabbas.cityweather.data
 
+import com.google.gson.Gson
 import com.mohdabbas.cityweather.data.source.local.LocalDataSource
+import com.mohdabbas.cityweather.data.source.remote.RemoteDataSource
+import com.mohdabbas.cityweather.util.GZIPUtil
+import com.mohdabbas.cityweather.util.InternalStorageReadWriteUtil
 import com.mohdabbas.cityweather.util.JsonParser
 import javax.inject.Inject
 
@@ -9,11 +13,17 @@ import javax.inject.Inject
  *
  * @author Mohammad Abbas
  * @property localDataSource an instance of the [LocalDataSource] class
+ * @property remoteDataSource an instance of the [RemoteDataSource] class
+ * @property internalStorageReadWrite an instance of the [InternalStorageReadWriteUtil] class
+ * @property gzipUtil an instance of the [GZIPUtil] class
  * @property jsonParser an instance of the [JsonParser] class.
  * @property mapper an instance of the [Mapper] class
  */
 class CityWeatherRepository @Inject constructor(
     private val localDataSource: LocalDataSource,
+    private val remoteDataSource: RemoteDataSource,
+    private val internalStorageReadWrite: InternalStorageReadWriteUtil,
+    private val gzipUtil: GZIPUtil,
     private val jsonParser: JsonParser,
     private val mapper: Mapper
 ) {
@@ -39,13 +49,29 @@ class CityWeatherRepository @Inject constructor(
     }
 
     /**
-     * Used to parse the list of [CityWeather] data from the Json file in the
-     * assets folder using [JsonParser] and mapped it to list of CityWeatherData, then update the
-     * database with the data
+     * Used to download the zip file, extract the json file that contain the city weather data,
+     * and store it in the internal storage, then read again and parse it using [Gson] to a list
+     * of [CityWeather], then mapped the a list to [CityWeatherData] to be stored in the database.
      */
     private suspend fun updateCityWeatherDataInDb() {
-        val parsedData = jsonParser.parseJson("weather_14.json")
-        val mappedData = mapper.run { parsedData.toCityWeatherData() }
+
+        val gzipInputStream =
+            remoteDataSource.downloadCitiesWeatherDataAndGetResponseBody().body()?.byteStream()
+
+        val citiesWeather = gzipInputStream?.let { inputStream ->
+            val gZIPInputStream = gzipUtil.getGZIPInputStream(inputStream)
+
+            val ndJsonFilePath =
+                internalStorageReadWrite.writeFileToInternalStorage(gZIPInputStream)
+
+            val ndJsonFile = internalStorageReadWrite.readFileFromInternalStorage(ndJsonFilePath)
+
+            val jsonDataAsStringList = ndJsonFile.bufferedReader().readLines()
+
+            jsonDataAsStringList.map { Gson().fromJson(it, CityWeather::class.java) }
+        } ?: listOf()
+
+        val mappedData = mapper.run { citiesWeather.toCityWeatherData() }
         localDataSource.storeCitiesWeatherDataInDb(mappedData)
     }
 }
